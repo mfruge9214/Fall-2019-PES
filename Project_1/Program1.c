8a69c97c0094eea2edb97b3c1c981514ec022228
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2019-09-18 15:45:06 -0600

    Update Program1.c
    Mikes attempt at p1

--- Project_1/Program1.c
+++ Project_1/Program1.c
@@ -7,6 +7,7 @@ Tools: GCC99 library & GNU toolchain
 
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 #define OCT_SZ      8
 #define DEC_SS      10
@@ -14,7 +15,7 @@ Tools: GCC99 library & GNU toolchain
 
 #define NIBBLE      4
 #define BYTE        8
-#define 2BYTE       16
+#define TWOBYTE       16
 
 #define MAX_UINT4   15
 #define MIN_INT4    -8
@@ -29,169 +30,159 @@ struct input{
 	uint32_t Op_Size;
 };
 
-int oct_to_dec(int oct)
-{
-	/*
-	int dec = 0;
-	int digit;
-	int count = 0;
-
-	while(oct) {
-		digit = oct % 10;
-		for(i)
-	}
-	In progress    */
-}
-
-int question1(int val, int radix, int op_size)
-{
-
-	int DEC_VAL, ABS_VAL, temp, cnt;
-	int BIN_VAL[op_size] = {0}; 
-	int SOC_VAL[op_size] = {0};
-	int STC_VAL[op_size] = {0};
-	int SAM_VAL[op_size] = {0};
-
-	int MAX, MIN = 0;
-
-
-	/********** Check for errors **********/
-	uint8_t err_flag = no_error;
-
-	if((radix != OCT_SZ) && (radix != DEC_SZ) && (radix != HEX_SZ)) 
-	{
-		err_flag |= invalid_radix;
-	}
-	else 
-	{
-		/* Compute Decimal Value */
-		if(radix == OCT_SZ) 
-		{
-			DEC_VAL = oct_to_dec(val);
-		}
-		else 
-		{               
-			DEC_VAL = val;
-		}
-
-		if(op_size == NIBBLE) 
-		{
-			if((DEC_VAL > MAX_UINT4) || (DEC_VAL < MIN_INT4)) 
-			{
-				err_flag |= invalid_val;
-			}
-			MAX = MAX_UINT4;
-		}
-		else if(op_size == BYTE) {
-			if((DEC_VAL > MAX_UINT8) || (DEC_VAL < MIN_INT8)) 
-			{
-				err_flag |= invalid_val;
-			}
-			MAX = MAX_UINT8;
-		}
-		else if(op_size == 2BYTE) {
-			if((DEC_VAL > MAX_UINT16) || (DEC_VAL < MIN_INT16)) 
-			{
-				err_flag |= invalid_val;
-			}
-			MAX = MAX_UINT16;
-		}
-		else {
-			err_flag |= invalid_opsize;
-		}
-	}
-
-	if(err_flag) 
-	{
-		printf("Invalid Input(s) to function:\n");
-		if(err_flag & invalid_radix) 
-		{
-			printf("  Invalid radix value (only expects input of 8, 10, or 16)\n");
-		}
-		if(err_flag & invalid_opsize) 
-		{
-			printf("  Invalid operand size (only expects input of 4, 8, or 16)\n");
-		}
-		if(err_flag & invalid_val)
-		{
-			printf("  Value is outside operand size bounds\n");
-		}
-
-		//exit function
-		return err_flag;
-	}
-
-
-	/********** Compute Values ************/
-	if(DEC_VAL < 0) 
-	{
-		ABS_VAL = -DEC_VAL;
-	}
-	else
-	{
-		ABS_VAL = DEC_VAL
-	}
-
-
-	temp = ABS_VAL;
-	cnt = 0;
-
-	// Binary (abs)
-	while(temp)
-	{
-		BIN_VAL[cnt] = temp % 2;
-		temp = temp / 2;
-		cnt++;
-	}
-
-
-	/*********** Print Table **********/
-	printf("Input:    Value %d,    radix %d,    operand size %d\n" val, radix, op_size");              //header1
-	printf("Output:                      Value                  Maximum                  Minimum\n");  //header2
-
-
-	//Binary (abs)
-	printf("Binary (abs)                 0b");
-	for(int i = (op_size - 1); i >= 0; i--) 
-	{
-		printf("%d", BIN_VAL[i]);
-	}
-
-	printf("                 0b");
-	for(int i = 0; i < op_size; i++)
-	{
-		printf("1");
-	}
-
-	printf("                 0b");
-	for(int i = 0; i < op_size; i++)
-	{
-		printf("0");
-	}
-	printf("\n");
-
-	//Octal (abs)
-	printf("Octal (abs)                  %o", ABS_VAL);
-	printf("                       %o                    %o\n", MAX, MIN);
-
-	//Decimal (abs)
-	printf("Decimal (abs)                %d", ABS_VAL);
-	printf("                       %d                    %d\n", MAX, MIN);
-
-	//Hexadecimal (abs)
-	printf("Hexadecimal (abs)            %x", ABS_VAL);
-	printf("                       %x                    %x\n", MAX, MIN);
-
-	//Signed One's Compliment
-
-	//Signed Two's Compliment 
-
-	//Sign-Magnitude
+uint32_t oct_to_dec(uint32_t oct);
 
 
 
-	return err_flag;
-}
+// int question1(int val, int radix, int op_size)
+// {
+
+// 	uint32_t DEC_VAL, ABS_VAL, temp, cnt;
+// 	uint32_t BIN_VAL[op_size] = {0}; 
+// 	uint32_t SOC_VAL[op_size] = {0};
+// 	uint32_t STC_VAL[op_size] = {0};
+// 	uint32_t SAM_VAL[op_size] = {0};
+
+// 	int MAX, MIN = 0;
+
+
+// 	/********** Check for errors **********/
+// 	uint8_t err_flag = no_error;
+
+// 	if((radix != OCT_SZ) && (radix != DEC_SZ) && (radix != HEX_SZ)) 
+// 	{
+// 		err_flag |= invalid_radix;
+// 	}
+// 	else 
+// 	{
+// 		/* Compute Decimal Value */
+// 		if(radix == OCT_SZ) 
+// 		{
+// 			DEC_VAL = oct_to_dec(val);
+// 		}
+// 		else 
+// 		{               
+// 			DEC_VAL = val;
+// 		}
+
+// 		if(op_size == NIBBLE) 
+// 		{
+// 			if((DEC_VAL > MAX_UINT4) || (DEC_VAL < MIN_INT4)) 
+// 			{
+// 				err_flag |= invalid_val;
+// 			}
+// 			MAX = MAX_UINT4;
+// 		}
+// 		else if(op_size == BYTE) {
+// 			if((DEC_VAL > MAX_UINT8) || (DEC_VAL < MIN_INT8)) 
+// 			{
+// 				err_flag |= invalid_val;
+// 			}
+// 			MAX = MAX_UINT8;
+// 		}
+// 		else if(op_size == TWOBYTE) {
+// 			if((DEC_VAL > MAX_UINT16) || (DEC_VAL < MIN_INT16)) 
+// 			{
+// 				err_flag |= invalid_val;
+// 			}
+// 			MAX = MAX_UINT16;
+// 		}
+// 		else {
+// 			err_flag |= invalid_opsize;
+// 		}
+// 	}
+
+// 	if(err_flag) 
+// 	{
+// 		printf("Invalid Input(s) to function:\n");
+// 		if(err_flag & invalid_radix) 
+// 		{
+// 			printf("  Invalid radix value (only expects input of 8, 10, or 16)\n");
+// 		}
+// 		if(err_flag & invalid_opsize) 
+// 		{
+// 			printf("  Invalid operand size (only expects input of 4, 8, or 16)\n");
+// 		}
+// 		if(err_flag & invalid_val)
+// 		{
+// 			printf("  Value is outside operand size bounds\n");
+// 		}
+
+// 		//exit function
+// 		return err_flag;
+// 	}
+
+
+// 	/********** Compute Values ************/
+// 	if(DEC_VAL < 0) 
+// 	{
+// 		ABS_VAL = -DEC_VAL;
+// 	}
+// 	else
+// 	{
+// 		ABS_VAL = DEC_VAL
+// 	}
+
+
+// 	temp = ABS_VAL;
+// 	cnt = 0;
+
+// 	// Binary (abs)
+// 	while(temp)
+// 	{
+// 		BIN_VAL[cnt] = temp % 2;
+// 		temp = temp / 2;
+// 		cnt++;
+// 	}
+
+
+// 	/*********** Print Table **********/
+// 	printf("Input:    Value %d,    radix %d,    operand size %d\n" val, radix, Op_Size);              //header1
+// 	printf("Output:                      Value                  Maximum                  Minimum\n");  //header2
+
+
+// 	//Binary (abs)
+// 	printf("Binary (abs)                 0b");
+// 	for(int i = (op_size - 1); i >= 0; i--) 
+// 	{
+// 		printf("%d", BIN_VAL[i]);
+// 	}
+
+// 	printf("                 0b");
+// 	for(int i = 0; i < op_size; i++)
+// 	{
+// 		printf("1");
+// 	}
+
+// 	printf("                 0b");
+// 	for(int i = 0; i < op_size; i++)
+// 	{
+// 		printf("0");
+// 	}
+// 	printf("\n");
+
+// 	//Octal (abs)
+// 	printf("Octal (abs)                  %o", ABS_VAL);
+// 	printf("                       %o                    %o\n", MAX, MIN);
+
+// 	//Decimal (abs)
+// 	printf("Decimal (abs)                %d", ABS_VAL);
+// 	printf("                       %d                    %d\n", MAX, MIN);
+
+// 	//Hexadecimal (abs)
+// 	printf("Hexadecimal (abs)            %x", ABS_VAL);
+// 	printf("                       %x                    %x\n", MAX, MIN);
+
+// 	//Signed One's Compliment
+
+// 	//Signed Two's Compliment 
+
+// 	//Sign-Magnitude
+
+
+
+// 	return err_flag;
+// }
 
 
 int main(int argc, char const *argv[])
@@ -246,13 +237,15 @@ int main(int argc, char const *argv[])
 
 	int i=0;
 
+	uint32_t res = oct_to_dec(inputs[5].value);
+	printf("Octal: %o\n", inputs[5].value);
+	printf("Decimal pre funct %d\n",inputs[5].value);
+	printf("Decimal Post Convertion: %d\n", res);
+
 	for(i=0; i<11; i++)
 	{
-        /*
 		printf("Input:	Value %d	Radix %d		Operand Size %d\r\n", inputs[i].value, inputs[i].radix, inputs[i].Op_Size);
-		printf("Output:		Value 		Maximum		Minimum");
-        */
-        
+		printf("Output:		Value 		Maximum		Minimum\n");   
 	}
 	
 	return 0;
@@ -260,4 +253,15 @@ int main(int argc, char const *argv[])
 
 
 
-void printBinaryABS(int value)
+uint32_t oct_to_dec(uint32_t oct)
+{
+	uint32_t i = 0;
+	uint32_t decimal = 0;
+	while (oct != 0)				/* https://www.sanfoundry.com/c-program-convert-octal-decimal/ */
+    {
+        decimal =  decimal +(oct % 10)* pow(8, i++);
+        oct = oct / 10;
+    }
+
+    return decimal;
+}
